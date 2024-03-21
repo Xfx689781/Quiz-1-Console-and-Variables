7722540a43cf0059fcf154cfd4a92fcb7d0e195f
FuxuanX689781 <159183219+Xfx689781@users.noreply.github.com>
2024-03-21 13:27:36 -0400

    commit

--- Program 4.js
+++ Program 4.js
@@ -2,6 +2,7 @@ const prompt = require('prompt-sync')()
 //Program 4: Largest Number Finder
 let number1 = prompt("What is the first number?");//to input the first number
 let number2 = prompt("What is the second number?");//to input the second number
+
 if(number1>number2){//to compare the first two numbers
     let bigger= number1;
 }
@@ -9,6 +10,7 @@ else{
     let bigger= number2;
 }
 let number3 = prompt("What is the third number?");
+
 if(number3>bigger){//to compare the bigger number with the last number
     let biggest= number3;
 }
